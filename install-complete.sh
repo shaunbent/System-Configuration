14d8539bff75a1eb6f6f06439d120f2ea8d4de97
Shaun Bent <shaunbent@me.com>
2014-06-06 09:08:56 +0100

    Add install script and rename install complete script

---
+++ install-complete.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# run `sh install.sh`
+
+# alias zsh files
+ln -s ~/Google\ Drive/System\ Configuration/dot/.zshrc ~/.zshrc
+
+# alias stylestats
+ln -s ~/Google\ Drive/System\ Configuration/dot/.stylestatsrc ~/.stylestatsrc
+
+# alias git files
+ln -s ~/Google\ Drive/System\ Configuration/dot/.gitconfig ~/.gitconfig
+ln -s ~/Google\ Drive/System\ Configuration/dot/.gitignore_global ~/.gitignore_global
--- install.sh
+++ install.sh
@@ -1,13 +1,47 @@
-#! /bin/sh
+function install_xcode() {
+    xcode-select --install
+}
 
-# run `sh install.sh`
+function install_homebrew() {
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+}
 
-# alias zsh files
-ln -s ~/Google\ Drive/System\ Configuration/dot/.zshrc ~/.zshrc
+function install_brews() {
+    brew tap caskroom/homebrew-cask
 
-# alias stylestats
-ln -s ~/Google\ Drive/System\ Configuration/dot/.stylestatsrc ~/.stylestatsrc
+    brews=( git node chruby ruby-install phantomjs wget \
+            cmake terminal-notifier brew-cask siege )
 
-# alias git files
-ln -s ~/Google\ Drive/System\ Configuration/dot/.gitconfig ~/.gitconfig
-ln -s ~/Google\ Drive/System\ Configuration/dot/.gitignore_global ~/.gitignore_global
+    for item in "${brews[@]}"
+        do
+            brew install $item
+    done
+}
+
+function install_software() {
+    casks=( atom adobe-creative-cloud box-sync alfred appcleaner rightzoom cloudapp vlc \
+          xscope cleanmymac google-chrome firefox qlstephen qlmarkdown quicklook-json \
+          quicklook-csv betterzipql suspicious-package evernote filezilla google-drive \
+          imageoptim opera paparazzi remote-desktop-connection sequel-pro skype sourcetree \
+          vagrant virtualbox )
+
+    for item in "${casks[@]}"
+        do
+            brew cask install $item
+    done
+}
+
+function install_oh_my_zsh() {
+    wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+}
+
+install_xcode && \
+install_homebrew && \
+install_brews && \
+install_software && \
+install_oh_my_zsh && \
+
+echo "Bitch, I've installed all the software we can.
+Check the README to see if there is anything else.
+Don't forget to start Google Drive and login so it starts sync'ing files.
+Then execute install-complete.sh after Dropbox has finished sync'ing (see README for instructions)"
