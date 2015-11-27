9a09db94765c40e4221423d3ea9f30cc11132dd7
Shaun Bent <shaunbent@me.com>
2015-11-27 21:32:56 +0000

    Add a whole load of osx specific stuff

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ function install_xcode() {
 }
 
 function install_homebrew() {
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"§
 }
 
 function install_brews() {
@@ -16,6 +16,10 @@ function install_brews() {
         do
             brew install $item
     done
+}
+
+function install_node_packages() {
+    npm install -g bower grunt-cli jshint pure-prompt
 }
 
 function install_software() {
@@ -33,11 +37,65 @@ function install_oh_my_zsh() {
     wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 }
 
+function misc_osx_stuff() {
+    # Show status bar in Finder by default
+    defaults write com.apple.finder ShowStatusBar -bool true
+
+    # Use column view in all Finder windows by default
+    defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+    # Avoid creation of .DS_Store files on network volumes
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+    # Allow text selection in Preview/Quick Look
+    defaults write com.apple.finder QLEnableTextSelection -bool true
+
+    # Enable AirDrop over Ethernet and on unsupported Macs running Lion
+    defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+    # Expand the following File Info panes:
+    # “General”, “Open with”, and “Sharing & Permissions”
+    defaults write com.apple.finder FXInfoPanesExpanded -dict \
+    	General -bool true \
+    	OpenWith -bool true \
+    	Privileges -bool true
+
+    # Show icons for hard drives, servers, and removable media on the desktop
+    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+    # Use the system native print preview dialog in Chrome
+    defaults write com.google.Chrome DisablePrintPreview -bool true
+    defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+    # Expand save panel by default
+    defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+    defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+    # Expand print panel by default
+    defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+    defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+    # Save to disk (not to iCloud) by default
+    defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+    # Automatically quit printer app once the print jobs complete
+    defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+    # Setup the bottom left hotcorner to sleep the display
+    defaults write com.apple.dock wvous-bl-corner -int 10
+    defaults write com.apple.dock wvous-bl-modifier -int 0
+}
+
 install_xcode && \
 install_homebrew && \
 install_brews && \
+install_node_packages && \
 install_software && \
 install_oh_my_zsh && \
+misc_osx_stuff && \
 
 echo "Bitch, I've installed all the software we can.
 Check the README to see if there is anything else.
